2ee8359defe542437271cc2984eb547afb0a894c
Mum <legonexoknights123@gmail.com>
2025-03-15 11:08:21 +0000

    upd

--- index.js
+++ index.js
@@ -3,11 +3,24 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 
+let psw ;
+let user ;
+let host ;
+let db ;
+
+$.getJSON("login.json", function(json) {
+     psw = json.get("psw");
+     user = json.get("usr");
+     host = json.get("host");
+     db = json.get("db");
+});
+
+
 const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'nodelogin'
+    host     : host,
+    user     : user,
+    password : psw,
+    database : db
 });
 
 const app = express();
@@ -22,8 +35,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'static')));
 
 // http://localhost:3000/
-app.get('/', function(request, response) {
+app.get('/login', function(request, response) {
     // Render login template
+    let name = request.get("name");
+    let secret = request.get("psw");
+    if(name!=null&&secret!=null) {
+        connection.query('SELECT * FROM logins WHERE name = ? AND password = ?', [name, secret], function(error, results, fields) {
+            if (error) throw error;
+            if (results.length > 0) {
+                response.send("true");
+            } else {
+                response.send("false");
+            }
+            response.end();
+        });
+    } else {
+        response.send('no_data');
+        response.end();
+    }
+
     response.sendFile(path.join(__dirname + '/login.html'));
 });
+app.listen(3000);
 
