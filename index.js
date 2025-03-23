369b64cd7c295fc9ac1c41643bb997d5be8d954e
Mum <legonexoknights123@gmail.com>
2025-03-23 16:47:28 +0000

    minor change

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ export default function handler(req,res) {
             });
         } else {
             return res.json({
-                success: 'false',
+                currentCape: 'empty'
             });
         }
     } else {
